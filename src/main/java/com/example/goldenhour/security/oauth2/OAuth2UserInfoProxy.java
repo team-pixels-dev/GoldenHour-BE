917ac12997cd8240fda1db2f8dfed82480ec29cb
Yun-YeoJun <yyj8771@naver.com>
2024-08-11 19:11:11 +0900

    CustomSuccessHandler 삭제 & OAuth2UserInfoProxy 설명 추가

--- src/main/java/com/example/goldenhour/security/oauth2/OAuth2UserInfoProxy.java
+++ src/main/java/com/example/goldenhour/security/oauth2/OAuth2UserInfoProxy.java
@@ -4,5 +4,10 @@ import com.example.goldenhour.dto.OAuth2Response;
 
 public interface OAuth2UserInfoProxy {
 
+    /**
+     * OAuth2 인증 서버에 액세스 토큰으로 요청을 보내서 유저 정보 획득 & 반환
+     * @param accessToken
+     * @return 유저 정보를 담은 OAuth2Response
+     */
     public OAuth2Response getOAuth2UserInfo(String accessToken);
 }
